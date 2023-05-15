54537b882155896ba3bda7f3f445951eec417685
jmikk <buldog.buldog26@gmail.com>
2023-05-15 08:11:14 -0500

    Update flag_upload.py

--- Flag uploader/flag_upload.py
+++ Flag uploader/flag_upload.py
@@ -6,16 +6,13 @@ UA = input("Please enter your nation's name")
 session = NSSession("Flag_uploader", "1.0.0", "Written by 9003",UA)
 reader = []
 
-if session.login("cardmania9007", "me"):  # logs in and checks if login was successful
-    session.move_to_region("Mordor")  # only moves if you successfully logged in
-
-# with open("flag_list.csv", "r") as csvfile:
-#    reader = csv.reader(csvfile)
-#    for each in reader:
-#        # logs in and checks if login was successful
-#        if session.login(each[0], each[1]):
-#            input(each[3])
-#            session.change_nation_flag(each[3])
+ with open("flag_list.csv", "r") as csvfile:
+    reader = csv.reader(csvfile)
+    for each in reader:
+        # logs in and checks if login was successful
+        if session.login(each[0], each[1]):
+            input(each[3])
+            session.change_nation_flag(each[3])
 
 
 
